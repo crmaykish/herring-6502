1a1ff981afe6f2696d3026d596afe99e5687fa99
Colin Maykish <colin.maykish@keeptruckin.com>
2021-07-05 18:24:42 -0400

    move VIA code to C

--- firmware/monitor.c
+++ firmware/monitor.c
@@ -1,4 +1,20 @@
-#include "via.h"
+#include <peekpoke.h>
+
+#define VIA_PORTA 0x4001
+#define VIA_PORTB 0x4000
+#define VIA_DDRA 0x4003
+#define VIA_DDRB 0x4002
+
+static void VIA_Init()
+{
+    // Set all pins on Port B to outputs
+    POKE(VIA_DDRB, 0xFF);
+}
+
+static void VIA_Write(char c)
+{
+    POKE(VIA_PORTB, c);
+}
 
 static unsigned char c = 0;
 
@@ -8,8 +24,14 @@ int main()
 
     for (;;)
     {
-        VIA_Write(c);
+        VIA_Write(0b111 << c);
+
         c++;
+
+        if (c == 8)
+        {
+            c = 0;
+        }
     }
 
     return 0;
--- firmware/via.h
+++
@@ -1,7 +0,0 @@
-#ifndef VIA_H
-#define VIA_H
-
-extern void VIA_Init();
-extern void __fastcall__ VIA_Write(char c);
-
-#endif
