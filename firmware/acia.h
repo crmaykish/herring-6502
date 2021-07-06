81bdc66ce51ea789333534266976403c340a2fa1
Colin Maykish <colin.maykish@keeptruckin.com>
2021-07-05 23:52:40 -0400

    outline of a command/response terminal for monitor

--- firmware/acia.h
+++
@@ -1,8 +0,0 @@
-#ifndef ACIA_H
-#define ACIA_H
-
-extern void ACIA_Init();
-
-extern void __fastcall__ ACIA_Write(char c);
-
-#endif
--- firmware/monitor.c
+++ firmware/monitor.c
@@ -1,37 +1,138 @@
+#include <string.h>
 #include <peekpoke.h>
 
-#define VIA_PORTA 0x4001
-#define VIA_PORTB 0x4000
-#define VIA_DDRA 0x4003
-#define VIA_DDRB 0x4002
+// #define VIA_PORTA 0x4001
+// #define VIA_PORTB 0x4000
+// #define VIA_DDRA 0x4003
+// #define VIA_DDRB 0x4002
 
-static void VIA_Init()
+#define ACIA_DATA 0x6000
+#define ACIA_STATUS 0x6001
+#define ACIA_COMMAND 0x6002
+#define ACIA_CONTROL 0x6003
+
+#define SERIAL_BUFFER_SIZE 32
+
+// Global Variables
+static unsigned char serial_buffer[SERIAL_BUFFER_SIZE];
+static unsigned char serial_buffer_idx = 0;
+
+// static void VIA_Init()
+// {
+//     // Set all pins on Port B to outputs
+//     POKE(VIA_DDRB, 0xFF);
+// }
+
+// static void VIA_Write(char c)
+// {
+//     POKE(VIA_PORTB, c);
+// }
+
+static void ACIA_Init()
 {
-    // Set all pins on Port B to outputs
-    POKE(VIA_DDRB, 0xFF);
+    POKE(ACIA_STATUS, 0x00);
+    POKE(ACIA_COMMAND, 0x0B);
+    POKE(ACIA_CONTROL, 0x1E);
 }
 
-static void VIA_Write(char c)
+// TODO: fast calls
+
+static void ACIA_Write(char c)
 {
-    POKE(VIA_PORTB, c);
+    while ((PEEK(ACIA_STATUS) & 0x10) == 0)
+    {
+        // Wait for ACIA Tx ready flag
+    }
+
+    // Write the character to the ACIA
+    POKE(ACIA_DATA, c);
 }
 
-static unsigned char c = 0;
+static void ACIA_NewLine()
+{
+    ACIA_Write('\n');
+    ACIA_Write('\r');
+}
+
+static void ACIA_WriteLine(char *message)
+{
+    unsigned char i = 0;
+    while (message[i] != 0)
+    {
+        ACIA_Write(message[i]);
+        i++;
+    }
+
+    ACIA_NewLine();
+}
+
+static unsigned char ACIA_Read()
+{
+    while ((PEEK(ACIA_STATUS) & 0x08) == 0)
+    {
+        // Wait for ACIA Rx data
+    }
+
+    return PEEK(ACIA_DATA);
+}
+
+static void ACIA_ReadLine()
+{
+    for (;;)
+    {
+        serial_buffer[serial_buffer_idx] = ACIA_Read();
+
+        // echo back to the console
+        ACIA_Write(serial_buffer[serial_buffer_idx]);
+
+        if (serial_buffer[serial_buffer_idx] == '\n' || serial_buffer[serial_buffer_idx] == '\r')
+        {
+            serial_buffer_idx = 0;
+            return;
+        }
+
+        serial_buffer_idx++;
+
+        if (serial_buffer_idx == SERIAL_BUFFER_SIZE)
+        {
+            serial_buffer_idx = 0;
+            return;
+        }
+    }
+}
 
 int main()
 {
-    VIA_Init();
+    unsigned char i = 0;
+
+    ACIA_Init();
+
+    ACIA_WriteLine("><(((°> Welcome to Herring 6502! ><(((°>");
 
     for (;;)
     {
-        VIA_Write(0b111 << c);
+        ACIA_Write('>');
+        ACIA_Write(' ');
 
-        c++;
+        ACIA_ReadLine();
 
-        if (c == 8)
-        {
-            c = 0;
-        }
+        ACIA_Write('\n');
+        ACIA_WriteLine(serial_buffer);
+
+        // TODO: strncmp is blowing up
+        // if (strncmp(serial_buffer, "peek", 4))
+        // {
+        //     ACIA_WriteLine("PEEKING");
+        // }
+        // else if (strncmp(serial_buffer, "poke", 4))
+        // {
+        //     ACIA_WriteLine("POKING");
+        // }
+
+        // Clear the serial buffer
+        memset(serial_buffer, 0, SERIAL_BUFFER_SIZE);
+
+        i = 0;
     }
 
     return 0;
