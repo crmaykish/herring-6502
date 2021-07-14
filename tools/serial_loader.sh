de368bace58fd58c647d68cfe076af3608801e5e
Colin Maykish <colin.maykish@keeptruckin.com>
2021-07-14 17:17:40 -0400

    added loading bar to serial transfer script

--- tools/serial_loader.sh
+++ tools/serial_loader.sh
@@ -19,17 +19,27 @@ sleep $SLEEP_TIME
 INDEX=0
 FILE_SIZE=$(wc -c <${FILE})
 
+DIV=$(($FILE_SIZE / 10))
+
 echo "Program size: "$FILE_SIZE" bytes"
 
-echo "Writing..."
+echo -n "Transferring program: "
+
+echo -n "|"
 
 while [ $INDEX -lt $FILE_SIZE ]; do
     dd if=${FILE} of=${DEVICE} status=none bs=1 count=1 skip=$INDEX
 
     INDEX=$((INDEX + 1))
 
+    if [ $(($INDEX % $DIV)) == 0 ]; then
+        echo -n "-"
+    fi
+
     sleep $SLEEP_TIME
 done
+
+echo "|"
 
 echo -en '!' >${DEVICE}
 sleep $SLEEP_TIME
