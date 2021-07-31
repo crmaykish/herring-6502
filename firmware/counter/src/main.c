284fcb6cc294597bb21cfad4437cbfa06120cc75
Colin Maykish <colin.maykish@keeptruckin.com>
2021-07-31 19:10:02 -0400

    started work on disassembler

--- firmware/counter/src/main.c
+++ firmware/counter/src/main.c
@@ -3,20 +3,9 @@
 #include "herring.h"
 #include "acia.h"
 
-void print_hex(word w)
-{
-    char s[7] = {0};
-    utoa(w, s, 16);
-
-    if (w < 0x10)
-    {
-        putc('0');
-    }
-
-    print(s);
-}
-
+void disassemble(byte *program, word size);
 void memdump(word addr, byte rows);
+void print_hex(word w);
 
 int main()
 {
@@ -39,6 +28,10 @@ int main()
         {
             running = false;
         }
+        else if (strncmp(input, "da", 2) == 0)
+        {
+            disassemble((void *)0x1000, 100);
+        }
 
         print("\r\n");
     }
@@ -46,6 +39,19 @@ int main()
     print("Exiting.\r\n");
 
     return 0;
+}
+
+void print_hex(word w)
+{
+    char s[7] = {0};
+    utoa(w, s, 16);
+
+    if (w < 0x10)
+    {
+        putc('0');
+    }
+
+    print(s);
 }
 
 void memdump(word addr, byte rows)
@@ -61,7 +67,7 @@ void memdump(word addr, byte rows)
             m = PEEK(addr + (16 * j) + i);
             print_hex(m);
             putc(' ');
-            
+
             if (i == 7)
             {
                 putc(' ');
@@ -69,5 +75,44 @@ void memdump(word addr, byte rows)
         }
 
         print("\r\n");
+    }
+}
+
+void disassemble(byte *program, word size)
+{
+    word i = 0;
+    byte bytes = 0;
+
+    while (i < size)
+    {
+        switch (program[i])
+        {
+        case 0xA9: // LDA #
+            print("lda #$");
+            print_hex(program[i + 1]);
+            bytes = 2;
+            break;
+
+        case 0xA5: // LDA zp
+            print("lda $");
+            print_hex(program[i + 1]);
+            bytes = 2;
+            break;
+
+        case 0x8D: // STA abs
+            print("sta $");
+            print_hex(program[i + 2]);
+            print_hex(program[i + 1]);
+            bytes = 3;
+            break;
+        default:
+            print("XX");
+            bytes = 2;
+            break;
+        }
+
+        print("\r\n");
+
+        i += bytes;
     }
 }
