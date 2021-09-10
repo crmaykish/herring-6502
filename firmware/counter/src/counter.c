8c8f105c1c8d9d6c7e66eed46919462ef6c200df
Colin Maykish <crmaykish@gmail.com>
2021-09-10 13:33:59 -0400

    Add demo program for FPGA GPIO latch

--- firmware/counter/src/counter.c
+++ firmware/counter/src/counter.c
@@ -2,14 +2,26 @@
 #include <peekpoke.h>
 #include "herring.h"
 
+void delay(word t)
+{
+    word delay = 0;
+
+    while (delay < t)
+    {
+        asm("nop");
+        delay++;
+    }
+}
+
 int main()
 {
-    word a = 0;
+    byte a = 0;
 
     while (true)
     {
-        POKEW(0x8400, a);
-
+        // Write the FPGA GPIO latch
+        POKE(0x8C00, a);
+        delay(1000);
         a++;
     }
 
--- hardware/gpio_controller/gpio_controller.v
+++ hardware/gpio_controller/gpio_controller.v
@@ -1,13 +1,16 @@
-module GPIO_Latch(DATA, CLK, EN, IO);
+module GPIO_Latch(DATA, CLK, EN, RST, IO);
     input [7:0] DATA;
-    input CLK, EN;
+    input CLK, EN, RST;
     output reg [7:0] IO;
 
     // Note: Neg edge triggering is a bit of a hack since the clock input is PHIO and not PHI2
     // Triggering on the posedge is too fast. The data bus is not ready yet.
-    always @(negedge CLK) begin
-        if (~EN)
-            IO <= DATA;
-    end
+    always @(negedge CLK or negedge RST)
+        begin
+            if (~RST)
+                IO <= 0;
+            else if (~EN)
+                IO <= DATA;
+        end
 
 endmodule
