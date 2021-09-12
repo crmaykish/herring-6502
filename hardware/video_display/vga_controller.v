cf072138f83943f693efa8a81594edcd382dc33a
Colin Maykish <crmaykish@gmail.com>
2021-09-12 14:11:33 -0400

    drawing rectangles demo

--- hardware/video_display/vga_controller.v
+++ hardware/video_display/vga_controller.v
@@ -1,48 +1,51 @@
-module vga_controller(R, G, B, Hs, Vs, CLK, LED);
+module vga_controller(R, G, B, Hs, Vs, CLK);
     input CLK;
-    output R, G, B, Hs, Vs, LED;
+    output R, G, B;
+    output Hs, Vs;
+    
+    reg [9:0] sx;
+    reg [9:0] sy;
 
-    reg [9:0] counter_x;
-    reg [8:0] counter_y;
-    wire x_max = (counter_x == 767);
+    wire de;
 
-    reg vga_hs, vga_vs;
-
-    reg [32:0] clk_count = 0;
-
-    wire clk_slow = clk_count[25];
-    wire clk_25mhz = clk_count[0];
+    // 25 MHz clock generation (from 50 MHz source clock)
+    reg pixel_clock = 1'b1;
 
     always @(posedge CLK) begin
-        clk_count <= clk_count + 1;
+        pixel_clock <= ~pixel_clock;
     end
 
-    always @(posedge clk_25mhz) begin
-        if (x_max)
-            counter_x <= 0;
+    parameter HA_END = 639;
+    parameter HS_STA = HA_END + 16;
+    parameter HS_END = HS_STA + 96;
+    parameter LINE = 799;
+
+    parameter VA_END = 479;
+    parameter VS_STA = VA_END + 10;
+    parameter VS_END = VS_STA + 2;
+    parameter SCREEN = 524;
+
+
+    always @(posedge pixel_clock) begin
+        if (sx == LINE)
+            begin
+                sx <= 0;
+                sy <= (sy == SCREEN) ? 0 : sy + 1;
+            end
         else
-            counter_x <= counter_x + 1;
+            begin
+                sx <= sx + 1;
+            end
     end
 
-    always @(posedge clk_25mhz) begin
-        if (x_max)
-            counter_y <= counter_y + 1;
-    end
+    assign Hs = ~(sx >= HS_STA && sx < HS_END);
+    assign Vs = ~(sy >= VS_STA && sy < VS_END);
+    assign de = (sx <= HA_END && sy <= VA_END);
 
-    always @(posedge clk_25mhz) begin
-        vga_hs <= (counter_x[9:4] == 0);
-        vga_vs <= (counter_y == 0);
-    end
+    assign R = (de && (sx < 32 && sy < 32)) ? 1 : 0;
 
+    assign G = (de && (sx > 600 && sy > 440)) ? 1 : 0;
 
-    assign Hs = ~vga_hs;
-    assign Vs = ~vga_vs;
-
-
-    assign R = counter_y[3] | (counter_x == 256);
-    assign G = (counter_x[5] ^ counter_x[6]) | (counter_x == 256);
-    assign B = counter_x[4] | (counter_x == 256);
-
-    assign LED = clk_slow;
+    assign B = (de && (sx > 400 && sx < 500) && sy > 300 && sy < 360) ? 1 : 0;
 
 endmodule
