6f9f43f18223e4e745aecb9a1a9e0e1244ed1ba1
Colin Maykish <crmaykish@gmail.com>
2021-09-17 21:56:31 -0400

    Broke out the VGA demo code from the timing module

--- hardware/herring_gpu/vga_timing.v
+++ hardware/herring_gpu/vga_timing.v
@@ -1,12 +1,12 @@
-module vga_timing(PIXEL_CLOCK, R, G, B, Hs, Vs);
+module vga_timing(PIXEL_CLOCK, R, G, B, Hs, Vs, SCREEN_X, SCREEN_Y, ON_SCREEN);
     input PIXEL_CLOCK;
     output reg R, G, B;
     output reg Hs, Vs;
 
-    reg [10:0] sx;
-    reg [9:0] sy;
+    output reg [10:0] SCREEN_X;
+    output reg [9:0] SCREEN_Y;
 
-    reg on_screen;
+    output reg ON_SCREEN;
 
     parameter HA_END = 799;
     parameter HS_STA = HA_END + 40;
@@ -19,31 +19,24 @@ module vga_timing(PIXEL_CLOCK, R, G, B, Hs, Vs);
     parameter SCREEN = 627;
 
     always @(posedge PIXEL_CLOCK) begin
-        if (sx == LINE)
+        if (SCREEN_X == LINE)
             begin
-                sx <= 0;
-                sy <= (sy == SCREEN) ? 0 : sy + 1;
+                SCREEN_X <= 0;
+                SCREEN_Y <= (SCREEN_Y == SCREEN) ? 0 : SCREEN_Y + 1;
             end
         else
             begin
-                sx <= sx + 1;
+                SCREEN_X <= SCREEN_X + 1;
             end
     end
 
     always @(posedge PIXEL_CLOCK) begin
-        // Set sync pulses
-        Hs <= (sx >= HS_STA && sx < HS_END);
-        Vs <= (sy >= VS_STA && sy < VS_END);
+        // Set SCREEN_Ync pulses
+        Hs <= (SCREEN_X >= HS_STA && SCREEN_X < HS_END);
+        Vs <= (SCREEN_Y >= VS_STA && SCREEN_Y < VS_END);
 
         // Is the pixel within the visible portion of the display?
-        on_screen <= (sx <= HA_END && sy <= VA_END);
-
-        if (on_screen)
-            begin
-                R <= (sx > 100 && sx < 200 && sy > 100 && sy < 200);
-                G <= (sx > 150 && sx < 250 && sy > 150 && sy < 250);
-                B <= (sx > 200 && sx < 300 && sy > 200 && sy < 300);
-            end
+        ON_SCREEN <= (SCREEN_X <= HA_END && SCREEN_Y <= VA_END);
     end
 
 endmodule
