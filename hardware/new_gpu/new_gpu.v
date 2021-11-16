b1519a575843c6154be9470d033ab8008e7a711b
Colin Maykish <crmaykish@gmail.com>
2021-11-16 17:12:12 -0500

    Demo pattern

--- hardware/new_gpu/new_gpu.v
+++ hardware/new_gpu/new_gpu.v
@@ -1,5 +1,5 @@
 module new_gpu(
-    input CLK_50MHz,
+    input CLK_PIXEL,
     output VGA_RED, VGA_GREEN, VGA_BLUE,
     output VGA_HSYNC, VGA_VSYNC
 );
@@ -7,15 +7,15 @@ module new_gpu(
     wire [10:0] pixel_y;
     wire on_screen;
 
-    vga_timing VGATiming(CLK_50MHz,
+    vga_timing VGATiming(CLK_PIXEL,
                          VGA_HSYNC,
                          VGA_VSYNC,
                          pixel_x,
                          pixel_y,
                          on_screen);
 
-    assign VGA_RED = on_screen & pixel_x < 100 & pixel_y < 100;
-    assign VGA_GREEN = on_screen & pixel_x < 200 & pixel_y < 200;
-    assign VGA_BLUE = on_screen & pixel_x < 400 & pixel_y < 400;
+    assign VGA_RED = on_screen && (((pixel_x & 7) == 0) || ((pixel_y & 7) == 0));
+    assign VGA_GREEN = on_screen && pixel_y[4];
+    assign VGA_BLUE = on_screen && pixel_x[4];
 
 endmodule
