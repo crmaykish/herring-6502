94d05932ca10c5d9be9b41393443884fd532216e
Colin Maykish <crmaykish@gmail.com>
2021-11-16 23:23:09 -0500

    Basic character fonts and screen buffer working

--- hardware/new_gpu/new_gpu.v
+++ hardware/new_gpu/new_gpu.v
@@ -17,44 +17,74 @@ module new_gpu(
     reg [24:0] clk_counter = 0;
     wire CLK_SLOW = clk_counter[22];
 
-    reg [4:0] sprite [0:4];
+    reg [7:0] sprite [0:2][0:7];
     reg [10:0] sprite_x = 20;
     reg [10:0] sprite_y = 40;
 
+
+    reg [1:0] framebuffer[0:99][0:74];
+
     initial begin
-        sprite[0] = 5'b10000;
-        sprite[1] = 5'b10000;
-        sprite[2] = 5'b11111;
-        sprite[3] = 5'b10001;
-        sprite[4] = 5'b10001;
+        // TODO: move font to a memory file
+        sprite[1][0] = 8'b11111111;
+        sprite[1][1] = 8'b10001111;
+        sprite[1][2] = 8'b10001111;
+        sprite[1][3] = 8'b10001111;
+        sprite[1][4] = 8'b11110001;
+        sprite[1][5] = 8'b11110001;
+        sprite[1][6] = 8'b11110001;
+        sprite[1][7] = 8'b11111111;
+
+        sprite[2][0] = 8'b10000001;
+        sprite[2][1] = 8'b01000010;
+        sprite[2][2] = 8'b00100100;
+        sprite[2][3] = 8'b00011000;
+        sprite[2][4] = 8'b00011000;
+        sprite[2][5] = 8'b00100100;
+        sprite[2][6] = 8'b01000010;
+        sprite[2][7] = 8'b10000001;
+
+        // NOTE: initializing the framebuffer seems very important
+        $readmemh("framebuffer.txt", framebuffer);
     end
 
-    always @(posedge CLK_PIXEL) begin
-        clk_counter <= clk_counter + 1;
-    end
+    // always @(posedge CLK_PIXEL) begin
+    //     clk_counter <= clk_counter + 1;
+    // end
 
-    always @(posedge CLK_SLOW) begin
-        sprite_x <= sprite_x + 2;
-        sprite_y <= sprite_y + 3;
+    // always @(posedge CLK_SLOW) begin
+    //     sprite_x <= sprite_x + 1;
+    //     sprite_y <= sprite_y + 1;
 
-        if (sprite_x >= 800) sprite_x <= 0;
-        if (sprite_y >= 600) sprite_y <= 0;
-    end
+    //     if (sprite_x >= 100) sprite_x <= 0;
+    //     if (sprite_y >= 75) sprite_y <= 0;
 
-    // enabled if the current VGA pixel is within the bounds of the sprite
-    wire in_sprite = pixel_x >= sprite_x && pixel_x < sprite_x + 5 && pixel_y >= sprite_y && pixel_y < sprite_y + 5;
+    //     // framebuffer[sprite_x][sprite_y] <= 1'b1;
+    // end
 
-    // indicies into the sprite table
-    wire [2:0] sp_x = pixel_x - sprite_x;
-    wire [2:0] sp_y = pixel_y - sprite_y;
+    // which framebuffer cell is the pixel in? is there a sprite there? if yes, find the pixel in the sprite
+
+    wire [7:0] fb_x = pixel_x[10:3];
+    wire [7:0] fb_y = pixel_y[10:3];
+
+    wire [2:0] sprite_index = framebuffer[fb_x][fb_y];
+
+    // TODO: This does seem to be required (at least as long as the memory is not BRAM)
+    wire in_sprite = (sprite_index > 0);
+
+    // indices into the sprite table
+    wire [2:0] sp_x = pixel_x - (fb_x << 3);
+    wire [2:0] sp_y = pixel_y - (fb_y << 3);
 
     // TODO: sprite is flipped horizontally
 
     // Is the exact pixel in the sprite on or off?
-    wire sprite_pixel = sprite[sp_y][sp_x];
+    wire sprite_pixel = sprite[sprite_index][sp_y][sp_x];
 
-    assign VGA_RED = on_screen && in_sprite && sprite_pixel;
-    // assign VGA_GREEN = on_screen && pixel_y[4];
+    assign VGA_RED = on_screen && sprite_index == 2 && sprite_pixel;
+    assign VGA_GREEN = on_screen && sprite_index == 1 && sprite_pixel;
     // assign VGA_BLUE = on_screen && pixel_x[4];
+
+    // TODO: currently, this is not using any BRAM. I think it's because all of this is combinatorial. BRAM probably needs a clock
 
 endmodule
