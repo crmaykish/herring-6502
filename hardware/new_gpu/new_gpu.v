4ca6973dde80d45e250993f987595335f4bdeb29
Colin Maykish <crmaykish@gmail.com>
2021-11-18 18:38:14 -0500

    More font characters

--- hardware/new_gpu/new_gpu.v
+++ hardware/new_gpu/new_gpu.v
@@ -22,7 +22,7 @@ module new_gpu(
                          pixel_y,
                          on_screen);
 
-    // Font ROM (127 x 8 x 8)
+    // Font ROM (64 x 8 x 8)
     reg [7:0] font [0:63][0:7];
 
     // Character buffer
@@ -33,45 +33,55 @@ module new_gpu(
         $readmemb("font.txt", font);
     end
 
-    // which framebuffer cell is the pixel in? is there a sprite there? if yes, find the pixel in the sprite
-
+    // Indices into the framebuffer
     wire [6:0] fb_x = pixel_x[10:3];
     wire [6:0] fb_y = pixel_y[10:3];
 
-    reg [6:0] sprite_index;
-
-    // indices into the sprite table
+    // Indices into the font table
     wire [2:0] sp_x = pixel_x - (fb_x << 3);
     wire [2:0] sp_y = pixel_y - (fb_y << 3);
 
-    // Is the exact pixel in the sprite on or off?
-    wire sprite_pixel = font[sprite_index][sp_y][sp_x ^ 3'b111];
-
-    assign VGA_GREEN = on_screen && sprite_pixel;
+    reg [6:0] sprite_index;
+    reg sprite_pixel;
 
     always @(posedge CLK_PIXEL) begin
         sprite_index <= framebuffer[fb_y][fb_x];
+        sprite_pixel <= font[sprite_index][sp_y][sp_x ^ 3'b111];
     end
+
+    // Assign the VGA signals to the current pixel in the framebuffer
+    assign VGA_GREEN = on_screen && sprite_pixel;
+
+
+    // === CPU interface and state machine === //
 
     reg [20:0] cursor_x = 0;
     reg [20:0] cursor_y = 0;
 
     reg [7:0] data_in = 0;
 
+    reg write_flag = 0;
+
+    // TODO build a state machine to handle I/O and more complex tasks
+
     always @(negedge CLK_CPU) begin
         if (~CE && ~RW) begin
             data_in <= DATA;
+            write_flag <= 1;
+        end
 
-            cursor_x <= cursor_x + 2;
+        if (write_flag) begin
+            framebuffer[cursor_y][cursor_x] <= data_in;
+
+            cursor_x <= cursor_x + 1;
 
             if (cursor_x == 78) begin
-                cursor_x <= 2;
+                cursor_x <= 0;
                 cursor_y <= cursor_y + 2;
             end
 
+            write_flag <= 0;
         end
-
-        framebuffer[cursor_y][cursor_x] <= data_in;
     end
 
 
