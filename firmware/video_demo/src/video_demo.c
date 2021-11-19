4504c33c748b6de4d06c0cc5691d7a7f9687daca
Colin Maykish <crmaykish@gmail.com>
2021-11-18 22:07:58 -0500

    Two versions of the GPU working

--- firmware/video_demo/src/video_demo.c
+++ firmware/video_demo/src/video_demo.c
@@ -1,21 +1,65 @@
+#include <string.h>
 #include <peekpoke.h>
 #include "herring.h"
 #include "acia.h"
 #include "print.h"
 
+#define FB_ADDR (0x9000)
+#define FB_START ((unsigned char*) (FB_ADDR))
+#define FB_SIZE (160 * 120)
+
+void delay(word t)
+{
+    word delay = 0;
+
+    while (delay < t)
+    {
+        asm("nop");
+        delay++;
+    }
+}
+
+void draw_block(byte x, byte y, byte w, byte h, byte color)
+{
+    
+}
+
 int main()
 {
-    char input = 0;
+    byte bg_color = 0x01;
+    byte player_color = 0x4;
+
+
+    byte pos_x = 0;
+    byte pos_y = 0;
+
+    byte sprite[] = {0x01, 0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02};
 
     acia_init();
 
-    print_line("Enter characters to send to the display: ");
+    print_line("Starting framebuffer demo");
+
+    print_line("Clearing framebuffer");
+    memset(FB_START, 0, FB_SIZE);
+    print_line("Done!");
+
+    print_line("Starting framebuffer demo...");
+
+    memcpy(FB_START + (pos_y * 160) + pos_x, sprite, sizeof(sprite));
 
     while (true)
     {
-        input = acia_getc();
+        // erase current sprite position
+        memset(FB_START + (pos_y * 160) + pos_x, 0, sizeof(sprite));
 
-        POKE(0x8C00, input - 64);
+        pos_x++;
+        pos_y++;
+
+        // redraw it at new position
+        memcpy(FB_START + (pos_y * 160) + pos_x, sprite, sizeof(sprite));
+
+        delay(20000);
+        delay(20000);
     }
 
     return 0;
