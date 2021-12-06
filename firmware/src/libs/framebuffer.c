0d44c12043b8292eaa62b23c297e3a335cc8fc59
Colin Maykish <crmaykish@gmail.com>
2021-12-05 19:29:00 -0500

    Removed old assembler

--- firmware/src/libs/framebuffer.c
+++ firmware/src/libs/framebuffer.c
@@ -34,30 +34,6 @@ void draw_pixel(byte x, byte y, byte color)
     POKE(pixel_address(x, y), color);
 }
 
-void draw_line(byte x1, byte y1, byte x2, byte y2, byte color)
-{
-    // THIS ONLY WORKS IN OCTANT 1
-    int x;
-    int y = y1;
-    int m_new = 2 * (y2 - y1);
-    int slope_error_new = m_new - (x2 - x1);
-    for (x = x1; x <= x2; x++)
-    {
-        draw_pixel(x, y, color);
-
-        // Add slope to increment angle formed
-        slope_error_new += m_new;
-
-        // Slope error reached limit, time to
-        // increment y and update slope error.
-        if (slope_error_new >= 0)
-        {
-            y++;
-            slope_error_new -= 2 * (x2 - x1);
-        }
-    }
-}
-
 void fill_rect(byte x, byte y, byte width, byte height, byte color)
 {
     byte i = 0;
