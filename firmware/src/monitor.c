7df9b02d9bc597c59cae63d40a89f79dcf4ea123
Colin Maykish <crmaykish@gmail.com>
2021-12-06 19:49:12 -0500

    Generalized handling of input commands in Herring Monitor

--- firmware/src/monitor.c
+++ firmware/src/monitor.c
@@ -10,114 +10,84 @@
 #include "jump.h"
 
 #define INPUT_BUFFER_SIZE 80
+#define COMMAND_MAX_LENGTH 6
+#define MEMDUMP_BYTES 128
 
-static const char *cpu_names[] = {"6502", "65C02", "65816", "4510", "65SC02", "65CE02"};
-
-static const char *delims = " ";
+typedef struct
+{
+    char name[6];
+    char param1[6];
+    char param2[6];
+    void (*handler)();
+} command_t;
 
 void header();
 void free_ram();
 void load_code(word addr);
 
+// Handler functions for each monitor command
+void handler_help();
+void handler_info();
+void handler_peek();
+void handler_poke();
+void handler_dump();
+void handler_jump();
+void handler_zero();
+void handler_load();
+void command_not_found(char *command_name);
+
+// CPU type names
+static const char *cpu_names[] = {"6502", "65C02", "65816", "4510", "65SC02", "65CE02"};
+
+// Input command definitions
+static const command_t commands[] = {
+    {"help", "", "", handler_help},
+    {"info", "", "", handler_info},
+    {"peek", "addr", "", handler_peek},
+    {"poke", "addr", "val", handler_poke},
+    {"dump", "addr", "", handler_dump},
+    {"jump", "addr", "", handler_jump},
+    {"zero", "addr", "", handler_zero},
+    {"load", "addr", "", handler_load}};
+
+static const byte COMMAND_COUNT = sizeof(commands) / sizeof(command_t);
+
 int main()
 {
+    byte i;
     char buffer[INPUT_BUFFER_SIZE];
     char *command;
-    char *param1;
-    char *param2;
-    word addr = 0;
-    byte val = 0;
+    bool command_handled;
 
     acia_init();
     header();
 
     while (true)
     {
+        command_handled = false;
+
+        // Present the command prompt and wait for input
         print("> ");
         readline(buffer, true);
         print_newline();
 
-        command = strtok(buffer, delims);
+        command = strtok(buffer, " ");
 
-        if (command != NULL)
+        // Look for the command name in the command list
+        for (i = 0; i < COMMAND_COUNT; i++)
         {
-            param1 = strtok(NULL, delims);
-
-            if (param1 != NULL)
+            if (strncmp(command, commands[i].name, COMMAND_MAX_LENGTH) == 0)
             {
-                param2 = strtok(NULL, delims);
+                // Found the command, handle it
+                commands[i].handler();
+                command_handled = true;
+                break;
             }
         }
 
-        if (strcmp(command, "help") == 0)
+        if (!command_handled)
         {
-            print_line("Commands:");
-            print_line("help");
-            print_line("clear");
-            print_line("info");
-            print_line("peek <addr>");
-            print_line("poke <addr> <val>");
-            print_line("dump <addr>");
-            print_line("jump <addr>");
-            print_line("load <addr>");
-            print("zero <addr>");
-        }
-        else if (strcmp(command, "peek") == 0)
-        {
-            addr = strtol(param1, 0, 16);
-            print_hex(PEEK(addr));
-        }
-        else if (strcmp(command, "poke") == 0)
-        {
-            addr = strtol(param1, 0, 16);
-            val = strtol(param2, 0, 16);
-            POKE(addr, val);
-            print("OK");
-        }
-        else if (strcmp(command, "dump") == 0)
-        {
-            addr = strtol(param1, 0, 16);
-            memdump(addr, 128);
-        }
-        else if (strcmp(command, "jump") == 0)
-        {
-            addr = strtol(param1, 0, 16);
-            jump_to(addr);
-        }
-        else if (strcmp(command, "zero") == 0)
-        {
-            addr = strtol(param1, 0, 16);
-            memset((word *)addr, 0, 128);
-
-            print("OK");
-        }
-        else if (strcmp(command, "info") == 0)
-        {
-            print("CPU: ");
-            print(cpu_names[getcpu()]);
-            print("\r\nRAM: ");
-            free_ram();
-        }
-        else if (strcmp(command, "clear") == 0)
-        {
-            screen_clear();
-            continue;
-        }
-        else if (strcmp(command, "load") == 0)
-        {
-            addr = strtol(param1, 0, 16);
-            print("Loading into: 0x");
-            print_hex(addr);
-            print_line("...");
-
-            load_code(addr);
-        }
-        else
-        {
-            font_red();
-            print("Command not found: ");
-            print(buffer);
-            font_reset();
+            command_not_found(command);
         }
 
         print_newline();
@@ -133,7 +103,7 @@ void header()
     print("=======================================================\r\n");
 
     font_green();
-    print("Herring Monitor v1.3 | Colin Maykish | Built 2021-12-01\r\n");
+    print("Herring Monitor v1.4 | Colin Maykish | Built 2021-12-06\r\n");
 
     font_cyan();
     print("github.com/crmaykish/herring-6502\r\n");
@@ -151,11 +121,98 @@ void free_ram()
     print(" bytes free");
 }
 
-void load_code(word addr)
+void handler_help()
+{
+    byte i = 0;
+
+    print_line("Available commands:");
+
+    for (i = 0; i < COMMAND_COUNT; i++)
+    {
+        print(commands[i].name);
+        if (commands[i].param1[0] != '\0')
+        {
+            print(" <");
+            print(commands[i].param1);
+            print(">");
+        }
+        if (commands[i].param2[0] != '\0')
+        {
+            print(" <");
+            print(commands[i].param1);
+            print(">");
+        }
+
+        if (i != COMMAND_COUNT - 1)
+        {
+            print_newline();
+        }
+    }
+}
+
+void handler_info()
+{
+    print("CPU: ");
+    print(cpu_names[getcpu()]);
+    print("\r\nRAM: ");
+    free_ram();
+}
+
+void handler_peek()
+{
+    char *param1 = strtok(NULL, " ");
+    word addr = strtol(param1, 0, 16);
+
+    print_hex(PEEK(addr));
+}
+
+void handler_poke()
+{
+    char *param1 = strtok(NULL, " ");
+    char *param2 = strtok(NULL, " ");
+
+    word addr = strtol(param1, 0, 16);
+    byte val = strtol(param2, 0, 16);
+
+    POKE(addr, val);
+    print("OK");
+}
+
+void handler_dump()
+{
+    char *param1 = strtok(NULL, " ");
+    word addr = strtol(param1, 0, 16);
+
+    memdump(addr, MEMDUMP_BYTES);
+}
+
+void handler_jump()
+{
+    char *param1 = strtok(NULL, " ");
+    word addr = strtol(param1, 0, 16);
+    jump_to(addr);
+}
+
+void handler_zero()
+{
+    char *param1 = strtok(NULL, " ");
+    word addr = strtol(param1, 0, 16);
+    memset((word *)addr, 0, MEMDUMP_BYTES);
+
+    print("OK");
+}
+
+void handler_load()
 {
     word in_count = 0;
     byte magic_count = 0;
     byte in = 0;
+    char *param1 = strtok(NULL, " ");
+    word addr = strtol(param1, 0, 16);
+
+    print("Loading into: 0x");
+    print_hex(addr);
+    print_line("...");
 
     while (magic_count != 3)
     {
@@ -181,5 +238,13 @@ void load_code(word addr)
         in_count++;
     }
 
-    print_line("Done loading!");
+    print("Done!");
+}
+
+void command_not_found(char *command_name)
+{
+    font_red();
+    print("Command not found: ");
+    print(command_name);
+    font_reset();
 }
