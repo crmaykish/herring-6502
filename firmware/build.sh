bb60cf76815212326b6b00a3cf3970c2d35864f8
Colin Maykish <crmaykish@gmail.com>
2021-12-14 17:22:13 -0500

    Add CHIP-8 project as a git submodule

--- firmware/build.sh
+++ firmware/build.sh
@@ -12,10 +12,12 @@ cl65 -O \
     --cpu 65c02 \
     --static-locals \
     -I include/ \
+    -I libs/chip8/include/ \
     --asm-include-dir include \
     -C config/${TYPE}.cfg \
     -o build/${TARGET}.bin \
     src/libs/* src/${TARGET}.c \
+    libs/chip8/src/chip8.c \
     config/${TYPE}.lib
 
 mv src/*.o build
